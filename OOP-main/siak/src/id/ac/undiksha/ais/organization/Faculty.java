d41ea9aea40609576d46bf8a0d1804e72e15d55c
Ananda Adi <127853079+AnandaAdi-2215101057@users.noreply.github.com>
2023-12-20 13:55:05 +0800

    Delete OOP-main/siak directory

--- OOP-main/siak/src/id/ac/undiksha/ais/organization/Faculty.java
+++
@@ -1,9 +0,0 @@
-package id.ac.undiksha.ais.organization;
-
-public interface Faculty {
-	public void setFacultyCode(String facultyCode);
-	public String getFacultyCode();
-	
-	public void setFacultyName(String facultyName);
-	public String getFacultyName();
-}
